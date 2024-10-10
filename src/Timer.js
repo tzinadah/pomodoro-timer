35c8d679b1a19fff0c146f3cb0b22893a824671a
Talal Abuziandah <primezinadah@gmail.com>
2024-10-10 14:00:55 +0100

    Add timer functionality

--- src/Timer.js
+++ src/Timer.js
@@ -1,13 +1,37 @@
+import { useEffect } from "react";
+
+import { decrementTimer, timerEnd } from "./timerSlice";
+import { useDispatch, useSelector } from "react-redux";
+
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 function Timer(props){
+
+    const dispatch = useDispatch();
+    
+    const seconds = useSelector((state) => state.timer.remainingSeconds);
+    const isRunning = useSelector((state) => state.timer.isRunning);
+    const currentSegment = useSelector((state) => state.timer.currentSegment)
+    
+    useEffect(()=>{
+        
+        if(seconds === 0){
+            dispatch(timerEnd);
+        }
+
+        if(isRunning){
+            setTimeout(()=> dispatch(decrementTimer), 1000);
+        }
+
+    },[isRunning,seconds]);
+
     return (
         <>
-        <Row><h2>Session</h2></Row>
+        <Row><h2>{currentSegment}</h2></Row>
         <Row>
             <Col>
-                25:00
+                {Math.floor(seconds/60).toString()}:{(seconds%60).toString().padStart(2, "0")}            
             </Col>
         </Row>
         </>
--- src/timerSlice.js
+++ src/timerSlice.js
@@ -1,43 +1,47 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const defaultState = {sessionLength: 25, breakLength: 5, remainingSeconds: 1500, isRunning: false};
+const defaultState = {sessionLength: 25, breakLength: 5, remainingSeconds: 1500, isRunning: false, currentSegment: "Session"};
 
 const timerSlice = createSlice({
     name: "timerSlice",
     initialState: defaultState,
     reducers: {
         
-        reset: (state) => {
-            return {...defaultState};
-        },
-        
         incrementSession: (state) => {
             return {...state, sessionLength: Math.min(99, state.sessionLength + 1), isRunning: false};
         },
-
+        
         decrementSession: (state) => {
             return {...state, sessionLength: Math.max(1, state.sessionLength - 1), isRunning: false};
         },
-
+        
         incrementBreak: (state) => {
             return {...state, breakLength: Math.min(99, state.breakLength + 1), isRunning: false};
         },
-
+        
         decrementBreak: (state) => {
             return {...state, breakLength: Math.max(1, state.breakLength - 1), isRunning: false};
+        },
+        
+        decrementTimer: (state) => {
+            return {...state, remainingSeconds: state.remainingSeconds - 60}
+        },
+        
+        timerEnd: (state) =>{
+            return {...state, remainingSeconds: state.currentSegment === "Session" ? state.breakLength : state.sessionLength}
         },
 
         togglePlay: (state) => {
             return {...defaultState};
         },
-
-        timerEnd: (state) =>{
+        
+        reset: (state) => {
             return {...defaultState};
         }
     }
 });
 
-export const {reset, incrementSession, decrementSession, incrementBreak, decrementBreak, togglePlay, timerEnd} = timerSlice.actions;
+export const {incrementSession, decrementSession, incrementBreak, decrementBreak, decrementTimer, timerEnd, togglePlay, reset} = timerSlice.actions;
 export default timerSlice.reducer;
 
 
